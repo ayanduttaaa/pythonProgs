fd1f504c6e2d3a4a0ee9107ec329cbb087b78603
ayanduttaaa <141127662+ayanduttaaa@users.noreply.github.com>
2023-08-05 12:14:28 +0530

    Update digitCount.py

--- digitCount.py
+++ digitCount.py
@@ -1,11 +1,8 @@
 #digit count of number
 num = int(input("Enter number : "))
-i = 0
 count = 0
-temp = num
-
-while temp > 0:
-    temp = temp // 10  
+while num > 0:
+    num = num // 10  
     count = count + 1
 
 print(f"The digit count of {num} is {count}")
